0ceb03cdbe83de985146a78fcea8b085b12db1e1
kiedunne <kie.dunne@gmail.com>
2019-01-14 12:25:50 +0000

    creates a statement

--- lib/account.rb
+++ lib/account.rb
@@ -2,19 +2,27 @@
 
 # Account class handles balance
 class Account
-  attr_reader :balance, :date
+  attr_reader :balance, :statement, :amount
 
   def initialize
-    @balance = 100
+    @balance = 0
     @amount = 0
-    @date = Time.now
+    @statement = []
   end
 
   def deposit(amount)
-    @balance += amount
+    @amount = amount
+    @balance += @amount
+    update_statement
   end
 
   def withdrawal(amount)
+    @amount = amount
     @balance -= amount
+    update_statement
+  end
+
+  def update_statement
+    @statement << [Time.now, @amount, @balance]
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,24 +5,27 @@ describe Account do
   let(:account) { described_class.new }
 
   it 'can print initial account balance' do
-    expect(account.balance).to eq 100
-  end
-
-  it 'can print time of a transaction' do
-    expect(account.date.class).to eq Time
+    expect(account.balance).to eq 0
   end
 
   describe '#deposit' do
     it 'can deposit money' do
-      account.deposit(50)
-      expect(account.balance).to eq 150
+      expect { account.deposit(50) }.to change { account.balance }.by(50)
     end
   end
 
   describe '#withdrawal' do
     it 'can withdrawal money' do
+      expect { account.withdrawal(50) }.to change { account.balance }.by(-50)
+    end
+  end
+
+  describe '#update_statement' do
+    it 'can store amount withdrawn, balance, and date in a transaction' do
       account.withdrawal(50)
-      expect(account.balance).to eq 50
+      expect(account.statement.flatten).to include(account.amount)
+      expect(account.statement.flatten).to include(account.balance)
+      expect(account.statement.flatten).to include(a_kind_of(Time))
     end
   end
 end

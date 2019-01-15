f9b0d82fee7e349131935af3f85c10c77c1cc920
kiedunne <kie.dunne@gmail.com>
2019-01-15 15:54:48 +0000

    rfctr Account SRP

--- lib/account.rb
+++ lib/account.rb
@@ -1,23 +1,30 @@
 # frozen_string_literal: true
-
+require_relative 'transaction'
 # Account class handles deposit, withdrawal and balance
 class Account
-  attr_reader :credit, :debit, :balance
+  attr_reader :credit, :debit, :balance, :transactions
   DEFAULT_AMOUNT = 0
 
   def initialize
-    @credit = DEFAULT_AMOUNT
-    @debit = DEFAULT_AMOUNT
+    @credit = 0
+    @debit = 0
     @balance = DEFAULT_AMOUNT
+    @transactions = []
   end
 
   def deposit(amount)
     @credit = amount
     @balance += amount
+    new_transaction
   end
 
   def withdrawal(amount)
     @debit = amount
     @balance -= amount
+    new_transaction
+  end
+
+  def new_transaction
+    @transactions << [@credit, @debit, @balance]
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -14,10 +14,19 @@ describe Account do
       expect { account.deposit(1000) }.to change { account.balance }.by(1000)
     end
   end
-  
+
   describe '#withdrawal' do
     it 'can make a withdrawal' do
       expect { account.withdrawal(500) }.to change { account.balance }.by(-500)
+    end
+  end
+  
+  describe '#new_transaction' do
+    it 'stores each withdrawal and deposit' do
+      account.deposit(1000)
+      account.deposit(2000)
+      account.withdrawal(500)
+      expect(account.transactions.length).to eq(3)
     end
   end
 end

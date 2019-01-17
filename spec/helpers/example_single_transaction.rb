44a2ed93d0a14b13ff3d930968b3ca55ea7d93b8
kiedunne <kie.dunne@gmail.com>
2019-01-17 16:01:01 +0000

    cleaned files

--- spec/helpers/example_single_transaction.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-def example_single_transaction
-  ['16/01/2019', '1000.00', '0.00', '1000.00']
-end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require_relative './helpers/example_single_transaction.rb'
 require 'account'
 require 'transaction'
 require 'timecop'
